2f5ea9e41f3305215d2532bd830663aceaaab214
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-09 15:44:18 +0100

    multiple io done with do

--- operators.hs
+++ operators.hs
@@ -1,3 +1,6 @@
 --main = putStrLn ("addition of two numbers : " ++ show (2+2)) -- dont require do beacuse show is not io action
-main = do putStrLn "addition of two numbers : " 
-          print (2+2) --  require do beacuse print is an io action. do only requires when we have multiple io actions: like putStrLn, print etc
+main = do -- multiple io operations
+    let var1 = 2
+    let var2 = 3
+    print(var1+var2)
+    print([1..3])
