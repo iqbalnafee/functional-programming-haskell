9d9ae87181b2faaa5ea8e56dceba42004b730dc9
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-09 21:50:21 +0100

    modules done

---
+++ modules.hs
@@ -0,0 +1,7 @@
+import Data.List
+import Data.Char
+main = do 
+    print(intersperse ',' "hello world")
+    print(sort [1,2,-1,0,100,40,200,5])
+    print(toUpper 'a')
+    print(words "Let us study tonight")
