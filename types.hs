112dd71fd6a82c873cc1d536215159dabe522443
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-09 16:50:26 +0100

    not equal to operator tested

--- types.hs
+++ types.hs
@@ -1,3 +1,8 @@
 fType:: Float -> Float -> Float
 fType x y = x*x + y*y
-main = print(fType 2 4)
+--main = print(fType 2 4)
+main =  do 
+    if 8 /= 8 -- /= equivalen to not equal to 
+        then putStrLn "this values are equivalent"
+    else 
+        putStrLn "they are not equivalent"
