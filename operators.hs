b911a2cfadaf276be64b020b8e3cb39292533556
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-09 15:41:27 +0100

    with do string shown

--- operators.hs
+++ operators.hs
@@ -1,1 +1,3 @@
-main = putStrLn ("addition of two numbers : " ++ show (2+2)) -- dont require do beacuse show is not io action
+--main = putStrLn ("addition of two numbers : " ++ show (2+2)) -- dont require do beacuse show is not io action
+main = do putStrLn "addition of two numbers : " 
+          print (2+2) --  require do beacuse print is an io action. do only requires when we have multiple io actions: like putStrLn, print etc
