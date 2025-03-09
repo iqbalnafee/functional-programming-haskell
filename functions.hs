17a0efd23944f4550e24f6672f21daed040eeb99
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-09 20:25:37 +0100

    guard added

--- functions.hs
+++ functions.hs
@@ -8,6 +8,15 @@ add x y = x + y
 fact :: Int -> Int
 fact 0 = 1
 fact n = n * fact (n-1)
+
+
+-- In patern matching we match one or more expressions,
+-- but in guard, it is used to test some property of an expression
+
+factGuard :: Integer -> Integer
+factGuard n | n == 0 = 1
+       | n /= 0 = n * factGuard(n-1)  
+
 main = do
-    putStrLn "The factorial of 5 is: "
-    print (fact 5)
+    putStrLn "The factorial of 5  using guard is: "
+    print (factGuard 5)
