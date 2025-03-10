2021d25edaa3a99d8a7011f1240beecaf9aeee98
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-10 15:29:13 +0100

    improve readbility

--- assignment-exercices/exercise4.1/basicHaskell.hs
+++ assignment-exercices/exercise4.1/basicHaskell.hs
@@ -11,4 +11,4 @@ main = do
     putStrLn("double of 5 is : " ++ show(double 5))
     putStrLn("ratio of 10+5 and 10-5 is is : " ++ show(ratio 10 5))
     putStrLn("length of the longest side of a right triangle using pythagoraus theorem: " ++ show(hypotenuse 2.5 3.3))
-    putStrLn("length of the longest side of a right triangle using pythagoraus theorem and lambda: " ++ show((\x -> \y -> sqrt(x^2 + y^2))3 4))
+    putStrLn("length of the longest side of a right triangle using pythagoraus theorem and lambda: " ++ show((\x y -> sqrt(x^2 + y^2))3 4))
