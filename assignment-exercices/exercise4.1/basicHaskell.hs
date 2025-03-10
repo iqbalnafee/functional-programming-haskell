3e1b8dcee9e2d63d20096fb5513835bdcb3e3469
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-10 15:08:15 +0100

    ratio problem solved

--- assignment-exercices/exercise4.1/basicHaskell.hs
+++ assignment-exercices/exercise4.1/basicHaskell.hs
@@ -1,4 +1,9 @@
 double :: Integer -> Integer
 double x = 2 * x
+
+ratio :: Float -> Float -> Float
+ratio x y = (x+y) / (x-y) 
+
 main = do
     putStrLn("double of 5 is : " ++ show(double 5))
+    putStrLn("ratio of 10+5 and 10-5 is is : " ++ show(ratio 10 5))
