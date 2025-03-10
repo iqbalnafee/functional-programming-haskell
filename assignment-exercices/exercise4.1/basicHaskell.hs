74515571c6db2858f81412ca38d80d5f6c061315
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-10 15:38:39 +0100

    xIntercept done

--- assignment-exercices/exercise4.1/basicHaskell.hs
+++ assignment-exercices/exercise4.1/basicHaskell.hs
@@ -7,8 +7,13 @@ ratio x y = (x+y) / (x-y)
 hypotenuse :: Double -> Double -> Double
 hypotenuse a b = sqrt (a^2+b^2) 
 
+xIntercept :: Float -> Float -> Float -> Float
+xIntercept _ _ 0 = error "Slope cannot be zero!"
+xIntercept y c m = (y-c) / m
+
 main = do
     putStrLn("double of 5 is : " ++ show(double 5))
-    putStrLn("ratio of 10+5 and 10-5 is is : " ++ show(ratio 10 5))
+    putStrLn("ratio of 10+5 and 10-5 is : " ++ show(ratio 10 5))
     putStrLn("length of the longest side of a right triangle using pythagoraus theorem: " ++ show(hypotenuse 2.5 3.3))
     putStrLn("length of the longest side of a right triangle using pythagoraus theorem and lambda: " ++ show((\x y -> sqrt(x^2 + y^2))3 4))
+    putStrLn("x-intercept of the line is : " ++ show(xIntercept 5 3 6))
