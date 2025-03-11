b4f6648fecdd0105c0c880fc289a9ab08c204163
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-11 15:21:24 +0100

    ord and num type issue understand

--- map.hs
+++ map.hs
@@ -1,4 +1,4 @@
-squareAll :: [Int] -> [Int]
+squareAll :: Num a => [a] -> [a] -- depends on the operations we are doing
 squareAll [] = []
 squareAll (h:ts) = (h^2) : squareAll ts -- In Haskell, lists are recursively defined using the cons (:) operator.
 -- [1,2,3] == 1 : (2 : (3 : []))
