cf6d92100fa4bef7dc5c0fd19fa028ec146053f5
Nafees Iqbal <iqbalnafee@gmail.com>
2025-03-11 17:43:59 +0100

    indentation done

--- io.hs
+++ io.hs
@@ -3,3 +3,8 @@ main = do
     putStrLn ("What's your name: ")
     name <- getLine
     putStrLn("Hello " ++ name)
+    putStrLn "Give me a number!"
+    num <- readLn :: IO Int
+    if (even num) then
+        putStrLn (show num ++ " is even.")
+    else putStrLn "odd number."
